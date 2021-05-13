8e16723f2e027e8441a7fd00fa8027db0c412ae0
Luis Pedraza <luispedraza@gmail.com>
2021-05-13 17:13:28 +0200

    creada la prueba para la función suma

---
+++ src/calculadora.js
@@ -0,0 +1,15 @@
+
+// Prueba de la función suma
+const prueba_suma = () => {
+    // Probar que 2 + 5 devuelve 7
+    // arrange
+    const a = 2;
+    const b = 5;
+    const esperado = 7;
+    // act
+    const resultado = suma(a, b);
+    // assert
+    console.assert(resultado === esperado, "fallo en la función suma");
+}
+
+prueba_suma();
